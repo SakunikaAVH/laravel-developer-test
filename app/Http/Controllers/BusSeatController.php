a53be0d913a54c13cbf2022fae16106fbba3579e
SakunikaAVH <saku.thara@gmail.com>
2021-06-17 14:47:40 +0530

    Created BusSeat model related files

---
+++ app/Http/Controllers/BusSeatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BusSeat;
+use Illuminate\Http\Request;
+
+class BusSeatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BusSeat  $busSeat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BusSeat $busSeat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BusSeat  $busSeat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BusSeat $busSeat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BusSeat  $busSeat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BusSeat $busSeat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BusSeat  $busSeat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BusSeat $busSeat)
+    {
+        //
+    }
+}
