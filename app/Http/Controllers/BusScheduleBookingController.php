e6444d7091589cdaded230870f08d6d4e9e83e72
SakunikaAVH <saku.thara@gmail.com>
2021-06-17 12:36:54 +0530

    Created BusScheduleBooking model related files

---
+++ app/Http/Controllers/BusScheduleBookingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BusScheduleBooking;
+use Illuminate\Http\Request;
+
+class BusScheduleBookingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BusScheduleBooking  $busScheduleBooking
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BusScheduleBooking $busScheduleBooking)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BusScheduleBooking  $busScheduleBooking
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BusScheduleBooking $busScheduleBooking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BusScheduleBooking  $busScheduleBooking
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BusScheduleBooking $busScheduleBooking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BusScheduleBooking  $busScheduleBooking
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BusScheduleBooking $busScheduleBooking)
+    {
+        //
+    }
+}
