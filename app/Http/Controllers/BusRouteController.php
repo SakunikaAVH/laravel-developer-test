77f4a3513b767505d0b5548c933e2b11ece82e91
SakunikaAVH <saku.thara@gmail.com>
2021-06-17 14:57:25 +0530

    Created BusRoute model related files

---
+++ app/Http/Controllers/BusRouteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BusRoute;
+use Illuminate\Http\Request;
+
+class BusRouteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BusRoute  $busRoute
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BusRoute $busRoute)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BusRoute  $busRoute
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BusRoute $busRoute)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BusRoute  $busRoute
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BusRoute $busRoute)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BusRoute  $busRoute
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BusRoute $busRoute)
+    {
+        //
+    }
+}
