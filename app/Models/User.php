c10be3148b6998d886093e34f8a4e194df9eb88a
SakunikaAVH <saku.thara@gmail.com>
2021-06-20 12:05:39 +0530

    Removed timestamp column from useres table and added seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -40,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function busScheduleBookings()
+    {
+        return $this->hasMany(BusScheduleBooking::class);
+    }
 }
