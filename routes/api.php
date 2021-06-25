b6e574539221a8426939dc2b97f34cff5a48a760
SakunikaAVH <saku.thara@gmail.com>
2021-06-25 23:40:13 +0530

    Created API endpoint for deleting users

--- routes/api.php
+++ routes/api.php
@@ -87,3 +87,5 @@ Route::get('/users', [UserController::class,'index']);
 Route::post('/users', [UserController::class,'store']);
 
 Route::patch('/users/{user}', [UserController::class,'update']);
+
+Route::delete('/users/{user}', [UserController::class,'destroy']);
