0c981bd09e617e5524a76f2ef69dddde35cdeb5f
SakunikaAVH <saku.thara@gmail.com>
2021-06-27 16:51:38 +0530

    Protected important routes using sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -26,74 +26,56 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Protected routes
-Route::group(['middleware' => [('auth:sanctum')]], function () {
-    Route::get('/users', [UserController::class,'index']);
-});
-
-
-Route::get('/buses', [BusController::class,'index']);
-
-Route::post('/buses', [BusController::class,'store']);
-
-Route::patch('/buses/{bus}', [BusController::class,'update']);
-
-Route::delete('/buses/{bus}', [BusController::class,'destroy']);
-
-Route::get('/bus_routes', [BusRouteController::class,'index']);
-
-Route::post('/bus_routes', [BusRouteController::class,'store']);
-
-Route::patch('/bus_routes/{bus_route}', [BusRouteController::class,'update']);
-
-Route::delete('/bus_routes/{bus_route}', [BusRouteController::class,'destroy']);
-
-Route::get('/bus_seats', [BusSeatController::class,'index']);
-
-Route::post('/bus_seats', [BusSeatController::class,'store']);
-
-Route::patch('/bus_seats/{bus_seat}', [BusSeatController::class,'update']);
-
-Route::delete('/bus_seats/{bus_seat}', [BusSeatController::class,'destroy']);
-
-Route::get('/routes', [RouteController::class,'index']);
-
-Route::post('/routes', [RouteController::class,'store']);
-
-Route::patch('/routes/{route}', [RouteController::class,'update']);
-
-Route::delete('/routes/{route}', [RouteController::class,'destroy']);
-
-Route::get('/bus_schedules', [BusScheduleController::class,'index']);
-
-Route::post('/bus_schedules', [BusScheduleController::class,'store']);
-
-Route::patch('/bus_schedules/{bus_schedule}', [BusScheduleController::class,'update']);
-
-Route::delete('/bus_schedules/{bus_schedule}', [BusScheduleController::class,'destroy']);
-
-Route::get('/bus_schedule_bookings', [BusScheduleBookingController::class,'index']);
-
-Route::post('/bus_schedule_bookings', [BusScheduleBookingController::class,'store']);
-
-Route::patch('/bus_schedule_bookings/{bus_schedule_booking}', [BusScheduleBookingController::class,'update']);
-
-Route::delete('/bus_schedule_bookings/{bus_schedule_booking}', [BusScheduleBookingController::class,'destroy']);
-
-Route::get('/super_admins', [SuperAdminController::class,'index']);
-
-Route::post('/super_admins', [SuperAdminController::class,'store']);
-
-Route::patch('/super_admins/{super_admin}', [SuperAdminController::class,'update']);
-
-Route::delete('/super_admins/{super_admin}', [SuperAdminController::class,'destroy']);
-
-Route::post('/users', [UserController::class,'store']);
-
-Route::patch('/users/{user}', [UserController::class,'update']);
-
-Route::delete('/users/{user}', [UserController::class,'destroy']);
-
+// Public routes
 Route::post('/register', [UserController::class,'register']);
 
 Route::post('/login', [UserController::class,'login']);
+
+Route::get('/bus_seats', [BusSeatController::class,'index']);
+
+Route::get('/bus_schedules', [BusScheduleController::class,'index']);
+
+
+// Protected routes
+Route::group(['middleware' => [('auth:sanctum')]], function () {
+    
+    Route::get('/users', [UserController::class,'index']);
+    Route::post('/users', [UserController::class,'store']);
+    Route::patch('/users/{user}', [UserController::class,'update']);
+    Route::delete('/users/{user}', [UserController::class,'destroy']);
+
+    Route::get('/super_admins', [SuperAdminController::class,'index']);
+    Route::post('/super_admins', [SuperAdminController::class,'store']);
+    Route::patch('/super_admins/{super_admin}', [SuperAdminController::class,'update']);
+    Route::delete('/super_admins/{super_admin}', [SuperAdminController::class,'destroy']);
+
+    Route::get('/bus_schedule_bookings', [BusScheduleBookingController::class,'index']);
+    Route::post('/bus_schedule_bookings', [BusScheduleBookingController::class,'store']);
+    Route::patch('/bus_schedule_bookings/{bus_schedule_booking}', [BusScheduleBookingController::class,'update']);
+    Route::delete('/bus_schedule_bookings/{bus_schedule_booking}', [BusScheduleBookingController::class,'destroy']);
+
+    Route::get('/buses', [BusController::class,'index']);
+    Route::post('/buses', [BusController::class,'store']);
+    Route::patch('/buses/{bus}', [BusController::class,'update']);
+    Route::delete('/buses/{bus}', [BusController::class,'destroy']);
+
+    Route::get('/bus_routes', [BusRouteController::class,'index']);
+    Route::post('/bus_routes', [BusRouteController::class,'store']);
+    Route::patch('/bus_routes/{bus_route}', [BusRouteController::class,'update']);
+    Route::delete('/bus_routes/{bus_route}', [BusRouteController::class,'destroy']);
+
+    Route::post('/bus_seats', [BusSeatController::class,'store']);
+    Route::patch('/bus_seats/{bus_seat}', [BusSeatController::class,'update']);
+    Route::delete('/bus_seats/{bus_seat}', [BusSeatController::class,'destroy']);
+
+    
+    Route::get('/routes', [RouteController::class,'index']);
+    Route::post('/routes', [RouteController::class,'store']);
+    Route::patch('/routes/{route}', [RouteController::class,'update']);
+    Route::delete('/routes/{route}', [RouteController::class,'destroy']);
+
+    Route::post('/bus_schedules', [BusScheduleController::class,'store']);
+    Route::patch('/bus_schedules/{bus_schedule}', [BusScheduleController::class,'update']);
+    Route::delete('/bus_schedules/{bus_schedule}', [BusScheduleController::class,'destroy']);
+
+});
